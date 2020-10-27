c5407d6f5a5fb48b153decc3ce328e05115b098f
Berkan Kütük <berkank@live.dk>
2020-10-27 11:27:51 +0100

    Added Inventory class

---
+++ semesterprojekt/src/worldofzuul/Inventory.java
@@ -0,0 +1,11 @@
+package worldofzuul;
+import worldofzuul.items.Item;
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Item> items = new ArrayList<Item>();
+
+    public static void addItem(Item item){
+
+    }
+}
