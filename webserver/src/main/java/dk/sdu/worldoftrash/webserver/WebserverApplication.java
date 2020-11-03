71014516aae77ea9a7d75cd41bddd464880f0bbc
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2020-11-03 09:14:07 +0100

    Added Server side rate limiting

--- webserver/src/main/java/dk/sdu/worldoftrash/webserver/WebserverApplication.java
+++ webserver/src/main/java/dk/sdu/worldoftrash/webserver/WebserverApplication.java
@@ -2,8 +2,10 @@ package dk.sdu.worldoftrash.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WebserverApplication {
 
 	public static void main(String[] args) {
