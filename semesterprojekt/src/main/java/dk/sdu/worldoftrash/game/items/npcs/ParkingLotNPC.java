fd05f260c64f8512cb42178d991fc2f4b5d8011e
Jan <jan1000dk@gmail.com>
2020-11-03 14:38:31 +0100

    Talk
    Added talk feature and some dialogue

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ParkingLotNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ParkingLotNPC.java
@@ -3,6 +3,8 @@ package dk.sdu.worldoftrash.game.items.npcs;
 import dk.sdu.worldoftrash.game.Game;
 import dk.sdu.worldoftrash.game.items.Item;
 
+import java.util.ArrayList;
+
 public class ParkingLotNPC extends NPC {
 
     public ParkingLotNPC(Game game, String name, String dialogue) {
