59a3d42f3a78179e77f446d483e73014d551931d
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2020-11-04 10:03:37 +0100

    NPC fix

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ZombieNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ZombieNPC.java
@@ -1,4 +1,21 @@
 package dk.sdu.worldoftrash.game.items.npcs;
 
-public class ZombieNPC {
+import dk.sdu.worldoftrash.game.Game;
+import dk.sdu.worldoftrash.game.items.Item;
+
+public class ZombieNPC extends NPC {
+
+    public ZombieNPC(Game game, String name, String dialogue) {
+        super(game, name, dialogue);
+    }
+
+    @Override
+    public void talk() {
+
+    }
+
+    @Override
+    public boolean giveItem(Item item) {
+        return false;
+    }
 }
