2bc1780169ebcbae3fefd3edbaf7cd3df5587964
Berkan Kütük <berkank@live.dk>
2020-11-08 15:02:19 +0100

    Parking lot dialogue

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ParkingLotNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ParkingLotNPC.java
@@ -13,7 +13,9 @@ public class ParkingLotNPC extends NPC {
 
     @Override
     public void talk() {
-        System.out.println("");
+        System.out.println("You! You in the green sweater! You must help me! I don’t have a place to stay and have never lived in an ordinary house before.");
+        System.out.println("You must find me a cardboard box, a big cardboard box, the biggest cardboard box, I want to be the king of the homeless! ");
+        System.out.println("Go to ‘supermarked’, they might have some cardboard boxes laying around, I'm sure you can find one in there somewhere");
     }
 
     @Override
