c9e8582b706456830150a0d5f3c9c3faeebb3fc1
Berkan Kütük <berkank@live.dk>
2020-11-08 15:21:37 +0100

    Added dialogue for two NPC's
    - School(Mad chemist)
    - Zombie(Mr. Zombie)

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/SchoolNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/SchoolNPC.java
@@ -14,8 +14,9 @@ public class SchoolNPC extends NPC {
 
     @Override
     public void talk() {
-        System.out.println("Lally-ho! What ya doin’ there laddie? Ya runnin’ favors for that twit Martin eh? If ye’re runnin’ favors mindin’ runnin’ me one too eh, laddie? Ya know, I’m working on fixin’ this place up and not just this mess of a school but Odense too. If ya steal me fertilizer from the janitor outside I’ma fix this place up real nice, pretty and everythin’.");
-
+        System.out.println("Lally-ho! What ya doin’ there laddie? Ya runnin’ favors for that twit Martin eh? If ye’re runnin’ favors mindin’ runnin’ me one too eh, laddie?");
+        System.out.println("Ya know, I’m working on fixin’ this place up and not just this mess of a school but Odense too");
+        System.out.println("If ya steal me fertilizer from the janitor outside I’ma fix this place up real nice, pretty and everythin’.");
     }
 
     @Override
--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ZombieNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/ZombieNPC.java
@@ -11,7 +11,9 @@ public class ZombieNPC extends NPC {
 
     @Override
     public void talk() {
-
+        System.out.println("RAAAAUUUUGHHHH. *Coughs * Sorry sir, but do you have a second to help me in this awkward situation?");
+        System.out.println("I accidentally lost one of my arms, and I could not avoid seeing you on the way to the HOSPITAL, so would you kindly search for my arm inside the hospital and if possible, also bring me a sewing kit so I can sew my arm on again?");
+        System.out.println("Thanks in advance.");
     }
 
     @Override
