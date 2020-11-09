d9aaf25e2cb94649a63e6107b0f7d5bc2017825e
Berkan Kütük <berkank@live.dk>
2020-11-09 11:24:05 +0100

    Relocated instructions for the "use" command
    - To Martin (NPC)

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/CityNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/items/npcs/CityNPC.java
@@ -21,15 +21,16 @@ public class CityNPC extends NPC {
         if (getGame().getCurrentRoom() == getGame().getSortingRoom()){
             System.out.println("This is our sorting room. In this room we have a lot of sorting bins, and it is your job to sort the waste you find in Odense");
             System.out.println("There are 8 different primary types of garbage, residual, hard plastic, hazardous, paper, cardboard, glass, metal and organic");
-            System.out.println("To sort your trash, use the DROP command as following (drop ‘item_name’ in ‘container_name’) and REMEMBER that some rubbish must be rinsed in SINK. Now meet me in ODENSE");
+            System.out.println("To sort your trash, use the DROP command as following (drop ‘item_name’ in ‘container_name’) and REMEMBER that some rubbish must be rinsed in SINK (Sink usage: use + sink + on + ITEM_NAME).");
+            System.out.println("Now meet me in ODENSE");
         }
         if (getGame().getCurrentRoom() == getGame().getOdense()){
             if (getGame().getScoreSystem().getWasteCount() >= 0 && getGame().getScoreSystem().getWasteCount() < 15) {
                 System.out.println("This is what's left of Odense after the trashpocalypse, and because you are new, I would recommend that you go to the SUPERMARKET first");
                 System.out.println("Once you are done there and have sorted the garbage, return to me.");
-                System.out.println("Btw, you have the ability to use the ‘search’ command if you aren’t able to find the trash in the different rooms.  ");
+                System.out.println("Btw, you have the ability to use the ‘search’ command if you aren’t able to find the trash in the different rooms.");
             } else if (getGame().getScoreSystem().getWasteCount() >= 15 && getGame().getScoreSystem().getWasteCount() < 30) {
-                System.out.println("Fantastic Job! You have now cleaned the SUPERMARKET and are now able to clean up the HOSPITAL. ");
+                System.out.println("Fantastic Job! You have now cleaned the SUPERMARKET and are now able to clean up the HOSPITAL.");
             } else if (getGame().getScoreSystem().getWasteCount() >= 30 && getGame().getScoreSystem().getWasteCount() < 45) {
                 System.out.println("This is incredible! I never thought the HOSPITAL was so white! I think you are ready for the most difficult part here in ODENSE, go to SCHOOL");
             }
