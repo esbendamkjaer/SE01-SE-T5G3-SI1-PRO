3a6d727a77d004cb09e1d14f7fa9e95eb7d46f95
Tobias Vedsted Sørensen <tobiasvedstedsorensen@gmail.com>
2020-12-04 11:24:18 +0100

    Also did an update on text

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/domain/items/npcs/CityNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/domain/items/npcs/CityNPC.java
@@ -12,30 +12,27 @@ public class CityNPC extends NPC {
     @Override
     public void talk() {
         if (getGame().getCurrentRoom().getName().equals("start")){
-            getGame().getTextPrinter().printText("\"Hello there! \uD83D\uDE09\"");
-            getGame().getTextPrinter().printText("\"Welcome to the World of Trash. My name is Trash Master Martin, but you can just call me Martin\"");
-            getGame().getTextPrinter().printText("\"You must help us save the planet! Now follow me into the next room if you want to survive. " +
+            getGame().getTextPrinter().printText("\"Hello there! \uD83D\uDE09\" \n\n\"Welcome to the World of Trash. My name is Trash Master Martin," +
+                    " but you can just call me Martin\" \n\n\"You must help us save the planet! Now follow me into the next room if you want to survive. " +
                     "You can go to the next room pressing X on the green arrow, and talk using X when you are near me\"");
         }
         if (getGame().getCurrentRoom().getName().equals("sortingRoom")){
-            getGame().getTextPrinter().printText("\"This is our sorting room. In this room we have a lot of sorting bins, and it is your job to sort the waste you find in Odense\"");
-            getGame().getTextPrinter().printText("\"There are 8 different primary types of garbage, residual, hard plastic, hazardous, paper, cardboard, glass, metal and organic\"");
-            getGame().getTextPrinter().printText("\"To sort your trash, walk up to the correct waste container and press LEFT CLICK on the WASTE in your INVENTORY MENU. " +
-                    "It is also important that you rinse your WASTE in the SINK\"");
-            getGame().getTextPrinter().printText("\"Now meet me outside in Odense\"");
+            getGame().getTextPrinter().printText("\"This is our sorting room. In this room we have a lot of sorting bins, and it is your job to sort the waste you find" +
+                    " in Odense\" \n\n\"There are 8 different primary types of garbage, residual, hard plastic, hazardous, paper, cardboard, glass, metal and" +
+                    " organic\" \n\n\"To sort your trash, walk up to the correct waste container and press LEFT CLICK on the WASTE in your INVENTORY MENU. " +
+                    "It is also important that you rinse your WASTE in the SINK\" \n\n\"Now meet me outside in Odense\"");
         }
         if (getGame().getCurrentRoom().getName().equals("city")){
             if (getGame().getScoreSystem().getWasteCount() >= 0 && getGame().getScoreSystem().getWasteCount() < 15) {
                 getGame().getTextPrinter().printText("\"This is what's left of Odense after the trashpocalypse, and because you are new, " +
-                        "I would recommend that you go to the SUPERMARKET in the east first\"");
-                getGame().getTextPrinter().printText("\"Once you are done there and have sorted the garbage, return to me\"");
-                getGame().getTextPrinter().printText("\"You might encounter survivors of the trashpocalypse and they’ll have important information to share if you help them." +
+                        "I would recommend that you go to the SUPERMARKET in the east first\" \n\n\"Once you are done there and have sorted the garbage," +
+                        " return to me\" \n\n\"You might encounter survivors of the trashpocalypse and they’ll have important information to share if you help them." +
                         " You can interact with them using X\"");
             } else if (getGame().getScoreSystem().getWasteCount() >= 15 && getGame().getScoreSystem().getWasteCount() < 30) {
                 getGame().getTextPrinter().printText("\"Fantastic Job! You have now cleaned the SUPERMARKET and are now able to clean up the HOSPITAL in south\"");
             } else if (getGame().getScoreSystem().getWasteCount() >= 30 && getGame().getScoreSystem().getWasteCount() < 45) {
-                getGame().getTextPrinter().printText("\"This is incredible! I never thought the HOSPITAL was so white! I think you are ready for the most difficult part here in ODENSE," +
-                        " go to SCHOOL in the north\"");
+                getGame().getTextPrinter().printText("\"This is incredible! I never thought the HOSPITAL was so white! I think you are ready for the most" +
+                        " difficult part here in ODENSE, go to SCHOOL in the north\"");
             }
         }
     }
