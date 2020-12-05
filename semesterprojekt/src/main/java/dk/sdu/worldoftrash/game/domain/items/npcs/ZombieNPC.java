09968feed1abb39d0a68b259c54479f983f96af3
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2020-12-05 11:15:41 +0100

    Changed print

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/domain/items/npcs/ZombieNPC.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/domain/items/npcs/ZombieNPC.java
@@ -34,17 +34,17 @@ public class ZombieNPC extends NPC {
             getInventory().storeItem(item);
             getGame().getTextPrinter().printText(
                     "\"Oh my if you aren't marching so gallantly with my arm under your arm\"" +
-                    (!getInventory().hasItems(sewing_kit) ? "\n\n\"If you'd get me a SEWING KIT, I would be eternally grateful\"" : ""));
+                    (!getInventory().hasItems(sewing_kit) ? "\n\n\"If you'd get me a SEWING KIT, I would be eternally grateful\"" : "") +
+                    checkItems());
 
-            checkItems();
             return true;
         } else if (item == sewing_kit) {
             getInventory().storeItem(item);
             getGame().getTextPrinter().printText(
                     "\"Thank you for the sewing kit!\"" +
-                    (!getInventory().hasItems(arm) ? "\n\n\"If you’d get me my ARM, I can sew it on\"" : ""));
+                    (!getInventory().hasItems(arm) ? "\n\n\"If you’d get me my ARM, I can sew it on\"" : "") +
+                    checkItems());
 
-            checkItems();
             return true;
         } else {
             getGame().getTextPrinter().printText("\"Are you joking with me?!\"");
@@ -52,13 +52,14 @@ public class ZombieNPC extends NPC {
         }
     }
 
-    public void checkItems() {
+    private String checkItems() {
         if (getInventory().hasItems(arm, sewing_kit)) {
-            getGame().getTextPrinter().printText(
-                    "\"Now I can right my wrongs of my past, Ignorance! Lazy! Reluctant! I wasn't the best trash sorter in my good days\"" +
-                    "\n\n\"My worst enemy actually were office supplies. Especially paper clips and post it notes. Did you know those two aren't worth recycling?\"" +
-                    "\n\n\"They’ll leave a larger carbon food print rather than just tossing them in with residual waste! You didn't? now you do chap\"");
+            return
+                "\n\n\"Now I can right my wrongs of my past, Ignorance! Lazy! Reluctant! I wasn't the best trash sorter in my good days\"" +
+                "\n\n\"My worst enemy actually were office supplies. Especially paper clips and post it notes. Did you know those two aren't worth recycling?\"" +
+                "\n\n\"They’ll leave a larger carbon food print rather than just tossing them in with residual waste! You didn't? now you do chap\"";
         }
+        return "";
     }
 
     public void setArm(Item arm) {
