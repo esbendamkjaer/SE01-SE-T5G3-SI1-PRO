780a90fe7ae5713b1f2c3d4539ee6b2ef861e8ea
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2020-12-07 11:20:36 +0100

    if else

--- semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/domain/Game.java
+++ semesterprojekt/src/main/java/dk/sdu/worldoftrash/game/domain/Game.java
@@ -946,7 +946,7 @@ public class Game {
         currentRoom = room;
         return true;
     }
-    
+
     /**
      * Called before every frame render.
      * @param delta Time since last frame.
